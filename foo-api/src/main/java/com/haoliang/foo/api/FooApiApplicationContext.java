692c84334ffdb79a8e4c9d9eca0df912fb62ce02
xiaoliangzi <xiaoliangzi@pinduoduo.com>
2017-10-29 22:48:24 +0800

    init

---
+++ foo-api/src/main/java/com/haoliang/foo/api/FooApiApplicationContext.java
@@ -0,0 +1,16 @@
+package com.haoliang.foo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xiaoliangzi on 2017/10/29.
+ */
+@SpringBootApplication
+public class FooApiApplicationContext {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooApiApplicationContext.class, args);
+    }
+
+}
---
+++ foo-api/src/test/java/com/haoliang/foo/api/BaseApiTest.java
@@ -0,0 +1,19 @@
+package com.haoliang.foo.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by xiaoliangzi on 2017/10/29.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseApiTest {
+
+    @Test
+    public void BaseTest() {
+    }
+
+}
